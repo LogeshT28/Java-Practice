2514063011fd25dab0abf7e603cd9ec8fd71912a
logesh028 <logesht2003@gmail.com>
2024-06-25 15:57:32 +0530

    Initialization

---
+++ Day  1/HellloWorld.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class HelloWorld{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+        System.out.println(a);
+    }
+}
