dc2e6c2414f1a367a70ab176ff25d00fa9095aff
logesh028 <logesht2003@gmail.com>
2024-07-29 19:33:31 +0530

    Practiced Remove Duplicate in linked list

---
+++ LinkedListLearning/LinkedListInsert.java
@@ -0,0 +1,86 @@
+package LinkedListLearning;
+
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class LinkedListInsert {
+    Node head;
+        LinkedListInsert() {
+            this.head=null;
+        }
+
+     void insertData(int data){
+        Node newnode = new Node(data);
+        if(head == null){
+            head = newnode;
+        }
+        else{
+            Node temp=head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            temp.next=newnode;
+        }
+    }
+
+    void display(){
+
+        Node temp= head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+
+    }
+
+    void reverse(){
+        Node pre=null;
+        Node curr=head;
+        Node next=null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next=pre;
+            pre=curr;
+            curr=next;
+        }
+        head = pre;
+    }
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    LinkedListInsert list =new LinkedListInsert();
+        while(true){
+            int choice = s.nextInt();
+            switch (choice) {
+                case 1:
+                    int data = s.nextInt();
+                    list.insertData(data);
+                    break;
+                case 2:
+                    list.display();
+                    break;
+
+                case 3:
+                    System.exit(0);
+                    break;
+                case 4:
+                    list.reverse();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+public void removeDup() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'removeDup'");
+}
+}
---
+++ LinkedListLearning/removeDuplicate.java
@@ -0,0 +1,122 @@
+package LinkedListLearning;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+    
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class removeDuplicate {
+    Node head;
+
+    removeDuplicate() {
+        this.head = null;
+    }
+
+    void insertData(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    void display() {
+        if (head == null) {
+            System.out.println("The list is empty.");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    void removeDup() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+        while (temp != null && temp.next != null) {
+            if (temp.data == temp.next.data) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+    }
+
+    void removeNotConsicuitiveElement(){
+        Node temp = head;
+        Node pre = null;
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        while(temp !=null){
+            if(set.contains(temp.data)){
+                pre.next = temp.next;
+            }
+            else{
+                set.add(temp.data);
+                pre =temp;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        removeDuplicate list = new removeDuplicate();
+        
+        while (true) {
+            System.out.println("1: Insert Data");
+            System.out.println("2: Display List");
+            System.out.println("3: Remove Duplicates");
+            System.out.println("4:Remove not Consicutive Elements");
+            System.out.println("5: Exit");
+            System.out.print("Enter your choice: ");
+            int choice = s.nextInt();
+            
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter data to insert: ");
+                    int data = s.nextInt();
+                    list.insertData(data);
+                    break;
+                case 2:
+                    list.display();
+                    break;
+                case 3:
+                    list.removeDup();
+                    break;
+                case 4:
+                    list.removeNotConsicuitiveElement();
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again.");
+                    break;
+            }
+        }
+    }
+}
